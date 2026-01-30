6a4a8218bc1bc7513f1663ea6ce3be5a125be56d
jasmine-se <jasmine.shihab.eddin@skandia.se>
2026-01-30 10:54:40 +0100

    Create OpenApiCmparer & method comparing two paths

---
+++ OpenApi.ContractGuard/Comparison/OpenApiComparer.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.OpenApi.Models;
+using OpenApi.ContractGuard.Comparison.enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace OpenApi.ContractGuard.Comparison
+{
+    public class OpenApiComparer
+    {
+        public List<ContractChange> Compare(
+            OpenApiDocument oldDoc,
+            OpenApiDocument newDoc)
+        {
+            var changes = new List<ContractChange>();
+
+             CheckPaths(oldDoc, newDoc, changes);
+
+            return changes;
+        }
+
+        private void CheckPaths(
+            OpenApiDocument oldDoc,
+            OpenApiDocument newDoc,
+            List<ContractChange> changes)
+        {
+            foreach (var newPath in newDoc.Paths)
+            {
+                if (!oldDoc.Paths.ContainsKey(newPath.Key))
+                {
+                    changes.Add(ContractChange.Create(
+                        ChangeType.PathAdded,
+                        $"Path added: {newPath.Key}",
+                        newPath.Key));
+                }
+            }
+
+            foreach (var oldPath in oldDoc.Paths)
+            {
+                if (!newDoc.Paths.ContainsKey(oldPath.Key))
+                {
+                    changes.Add(ContractChange.Create(
+                        ChangeType.PathRemoved,
+                        $"Path removed: {oldPath.Key}",
+                        oldPath.Key));
+                }
+            }
+        }
+
+    }
+}
