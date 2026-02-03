7fedd45b8dda9702294663acd11572aa29330b0d
jasmine-se <jasmine.shihab.eddin@skandia.se>
2026-02-03 09:44:09 +0100

    Add appsettings.json configuration support

--- OpenApi.ContractGuard/Program.cs
+++ OpenApi.ContractGuard/Program.cs
@@ -1,7 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
 
-var file1 = new OpenApiLoader().Load("C:\\projects\\APIS\\apis\\skandia\\internal\\individkund-risk-och-halsa\\riskbedomning-rest\\v1\\riskbedomning-rest.json");
-var file2 = new OpenApiLoader().Load("C:\\projects\\APIS\\apis\\skandia\\internal\\individkund-risk-och-halsa\\riskbedomning-rest\\v2\\riskbedomning-rest.json");
-var comparer = new OpenApi.ContractGuard.Comparison.OpenApiComparer();
-var changes = comparer.Compare(file1, file2);
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();
+
+Console.WriteLine(configuration["OpenApi:Old"]);
+Console.WriteLine(configuration["OpenApi:New"]);
+
+//var file1 = new OpenApiLoader().Load("C:\\projects\\APIS\\apis\\skandia\\internal\\individkund-risk-och-halsa\\riskbedomning-rest\\v1\\riskbedomning-rest.json");
+//var file2 = new OpenApiLoader().Load("");
+//var comparer = new OpenApi.ContractGuard.Comparison.OpenApiComparer();
+//6var changes = comparer.Compare(file1, file2);
