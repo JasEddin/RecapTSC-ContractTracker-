6d04c0ed3a62ca01a2c252a1171dfb6616ce1aef
jasmine-se <jasmine.shihab.eddin@skandia.se>
2026-02-03 11:05:47 +0100

    Implement print changes

--- OpenApi.ContractGuard/Program.cs
+++ OpenApi.ContractGuard/Program.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.Extensions.Configuration;
+using OpenApi.ContractGuard.Comparison;
+using OpenApi.ContractGuard.Reporting;
 
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();
 
-Console.WriteLine(configuration["OpenApi:Old"]);
-Console.WriteLine(configuration["OpenApi:New"]);
-
-//var file1 = new OpenApiLoader().Load("C:\\projects\\APIS\\apis\\skandia\\internal\\individkund-risk-och-halsa\\riskbedomning-rest\\v1\\riskbedomning-rest.json");
-//var file2 = new OpenApiLoader().Load("");
-//var comparer = new OpenApi.ContractGuard.Comparison.OpenApiComparer();
-//6var changes = comparer.Compare(file1, file2);
+ var file1 = new OpenApiLoader().Load(configuration["OpenApi:Old"]);
+ var file2 = new OpenApiLoader().Load(configuration["OpenApi:New"]);
+var comparer = new OpenApi.ContractGuard.Comparison.OpenApiComparer();
+var changes = comparer.Compare(file1, file2);
+ConsoleReporter.ReportChanges(changes);
---
+++ OpenApi.ContractGuard/Reporting/ConsoleReporter.cs
@@ -0,0 +1,74 @@
+﻿using OpenApi.ContractGuard.Comparison;
+using OpenApi.ContractGuard.Comparison.enums;
+
+namespace OpenApi.ContractGuard.Reporting
+{
+    public static class ConsoleReporter
+    {
+        public static void ReportChanges(IEnumerable<ContractChange> changes)
+        {
+            if (!changes.Any())
+            {
+                WriteSuccess("No contract changes detected.");
+                return;
+            }
+
+            WriteWarning("Detected Changes:");
+
+            if (changes.Any(change => change.Impact == ChangeImpact.ContractUpdateRequired))
+            {
+                // I want to make this more visible
+
+                WriteError("Contract changes detected that require an update to the contract.");
+            }
+
+            foreach (var change in changes)
+            {
+                WriteChange(change);
+            }
+        }
+
+        private static void WriteChange(ContractChange change)
+        {
+            switch (change.Impact)
+            {
+                case ChangeImpact.ContractUpdateRequired:
+                    WriteError($"{change.Message}");
+                    break;
+                case ChangeImpact.Informational:
+                    WriteWarning($"{change.Message}");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void WriteSuccess(string message)
+        {
+            var previousColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ForegroundColor = previousColor;
+        }
+
+        private static void WriteError(string message)
+        {
+            var previousColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = previousColor;
+        }
+
+        private static void WriteWarning(string message)
+        {
+            var previousColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(message);
+            Console.ForegroundColor = previousColor;
+        }
+    }
+}
+
+
+
+
